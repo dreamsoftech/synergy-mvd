9301f5fd5b189acefb376c123f54311135bb4087
dreamsoftech <dreamsoftech88@gmail.com>
2016-01-16 18:38:30 -0500

    Add comments in task detail modal

--- public/partials/action-items/task/task-modal.js
+++ public/partials/action-items/task/task-modal.js
@@ -8,6 +8,7 @@ app.controller('TaskModalCtrl',
             $uibModalInstance.close();
         }
         $scope.task = currentTask;
+        $scope.isNewModal = _.isUndefined(currentTask.title);
 
         $scope.isShowDatePicker = false;
         $scope.selectedDate = new Date(2015, 7, 18);
@@ -142,6 +143,60 @@ app.controller('TaskModalCtrl',
             {
                 id: 19,
                 name: "Snow Removal"
+            }
+        ];
+
+        $scope.comments = [
+            {
+                avatar: 'bruce.jpg',
+                author: 'Bruce Wayne',
+                date: 'May 20, 2015 - 3:40pm',
+                desc: 'This is a comment.  I appologize for the delayed response. My team and I have been slammed trying to complete the Roof Dry-In proccess by the set deadline.'
+            }, {
+                avatar: 'jeff.jpg',
+                author: 'Jeff Price',
+                date: 'May 20, 2015 - 3:40pm',
+                desc: 'This is a comment.  I appologize for the delayed response. My team and I have been slammed trying to complete the Roof Dry-In proccess by the set deadline.'
+            }, {
+                avatar: 'peters.jpg',
+                author: 'Christian Peters',
+                date: 'May 20, 2015 - 3:40pm',
+                desc: 'This is a comment.  I appologize for the delayed response. My team and I have been slammed trying to complete the Roof Dry-In proccess by the set deadline.'
+            }, {
+                avatar: 'jane.jpg',
+                author: 'Jane Smith',
+                date: 'May 20, 2015 - 3:40pm',
+                desc: 'This is a comment.  I appologize for the delayed response. My team and I have been slammed trying to complete the Roof Dry-In proccess by the set deadline.'
+            }, {
+                avatar: 'dan.jpg',
+                author: 'Dan Bonker',
+                date: 'May 20, 2015 - 3:40pm',
+                desc: 'This is a comment.  I appologize for the delayed response. My team and I have been slammed trying to complete the Roof Dry-In proccess by the set deadline.'
+            }, {
+                avatar: 'bruce.jpg',
+                author: 'Bruce Wayne',
+                date: 'May 20, 2015 - 3:40pm',
+                desc: 'This is a comment.  I appologize for the delayed response. My team and I have been slammed trying to complete the Roof Dry-In proccess by the set deadline.'
+            }, {
+                avatar: 'jeff.jpg',
+                author: 'Jeff Price',
+                date: 'May 20, 2015 - 3:40pm',
+                desc: 'This is a comment.  I appologize for the delayed response. My team and I have been slammed trying to complete the Roof Dry-In proccess by the set deadline.'
+            }, {
+                avatar: 'peters.jpg',
+                author: 'Christian Peters',
+                date: 'May 20, 2015 - 3:40pm',
+                desc: 'This is a comment.  I appologize for the delayed response. My team and I have been slammed trying to complete the Roof Dry-In proccess by the set deadline.'
+            }, {
+                avatar: 'jane.jpg',
+                author: 'Jane Smith',
+                date: 'May 20, 2015 - 3:40pm',
+                desc: 'This is a comment.  I appologize for the delayed response. My team and I have been slammed trying to complete the Roof Dry-In proccess by the set deadline.'
+            }, {
+                avatar: 'dan.jpg',
+                author: 'Dan Bonker',
+                date: 'May 20, 2015 - 3:40pm',
+                desc: 'This is a comment.  I appologize for the delayed response. My team and I have been slammed trying to complete the Roof Dry-In proccess by the set deadline.'
             }
         ];
 
