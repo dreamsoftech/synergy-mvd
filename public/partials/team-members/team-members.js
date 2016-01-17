6b84dfa0aade6e35a0cdb276d45ae65b4e5dcb2c
Bryan Lloyd <bryan.lloyd45@gmail.com>
2016-01-17 14:00:51 -0500

    Team Member - Multiple Hover Details

--- public/partials/team-members/team-members.js
+++ public/partials/team-members/team-members.js
@@ -1,4 +1,4 @@
-app.controller('TeamMembersCtrl',  ['$scope', '$stateParams', '$uibModal', function ($scope, $stateParams, $uibModal) {
+app.controller('TeamMembersCtrl',  ['$scope', '$stateParams', '$uibModal', '$sce', function ($scope, $stateParams, $uibModal, $sce) {
 
     $scope.test = "hello world";
 
@@ -27,4 +27,13 @@ app.controller('TeamMembersCtrl',  ['$scope', '$stateParams', '$uibModal', funct
 
         });
     }
+
+    $scope.mike_sow_popover = $sce.trustAsHtml("<div>Main Floor Overhead Lighting<br>Rough Electrical<br>Stairway Floor Lighting</div>");
+    $scope.dave_gebo_sow_popover = $sce.trustAsHtml("<div>Roof Dry-In<br>Set Steel</div>");
+    $scope.dave_gebo_project_popover = $sce.trustAsHtml("<div>Wilmington 47<br>Wilmington 48</div>");
+    $scope.bruce_wayne_project_popover = $sce.trustAsHtml("<div>Wilmington 45<br>Wilmington 46<br>Wilmington 47<br>Wilmington 48</div>");
+    $scope.bruce_wayne_sow_popover = $sce.trustAsHtml("<div>Frame 2nd Story<br>Insulation<br>Snow Removal</div>");
+    $scope.jeff_price_sow_popover = $sce.trustAsHtml("<div>Roof Dry-In<br>Set Steel</div>");
+    $scope.jeff_price_project_popover = $sce.trustAsHtml("<div>Wilmington 47<br>Wilmington 48</div>");
+
 }]);
