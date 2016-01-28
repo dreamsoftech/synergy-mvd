66c3eb0bfebc0c83a9bc01f4c8a7700062aa7609
dreamsoftech <dreamsoftech88@gmail.com>
2016-01-28 15:59:05 -0500

    Add constants

--- public/content/js/app.js
+++ public/content/js/app.js
@@ -805,6 +805,216 @@ app.constant("projects", [{
         zipcode: "90125"
     }]);
 
+app.constant("sows", 
+    [
+        {id: _.uniqueId(), name: 'Doors'},
+        {id: _.uniqueId(), name: 'Drywall'},
+        {id: _.uniqueId(), name: 'Fire Sprinkler Rough In'},
+        {id: _.uniqueId(), name: 'Fireplace'},
+        {id: _.uniqueId(), name: 'Form Footings'},
+        {id: _.uniqueId(), name: 'Form Foundation Walls'},
+        {id: _.uniqueId(), name: 'Foundation'},
+        {id: _.uniqueId(), name: 'Framing'},
+        {id: _.uniqueId(), name: 'Insulation'},
+        {id: _.uniqueId(), name: 'Lighting'},
+        {id: _.uniqueId(), name: 'Plumbing Rough In'},
+        {id: _.uniqueId(), name: 'Prime'},
+        {id: _.uniqueId(), name: 'Roof Dry In'},
+        {id: _.uniqueId(), name: 'Rough Electrical'},
+        {id: _.uniqueId(), name: 'Rough Mechanical'},
+        {id: _.uniqueId(), name: 'Rough Plumbing'},
+        {id: _.uniqueId(), name: 'Set Steel'},
+        {id: _.uniqueId(), name: 'Snow Removal'},
+        {id: _.uniqueId(), name: 'Stairs'},
+        {id: _.uniqueId(), name: 'Windows'}
+    ]);
+
+app.constant("contracts",
+    [
+        {id: _.uniqueId(), name: 'BID #1002 - Structural Metal'},
+        {id: _.uniqueId(), name: 'BID #1007 - Rough Carpentry'},
+        {id: _.uniqueId(), name: 'BID #1016 - Concrete'},
+        {id: _.uniqueId(), name: 'BID #1042 - Fire Protection'},
+        {id: _.uniqueId(), name: 'BID #1058 - Building Service'},
+        {id: _.uniqueId(), name: 'BID #1061 - HVAC'},
+        {id: _.uniqueId(), name: 'BID #1065 - Electrical'},
+        {id: _.uniqueId(), name: 'PO #1018 - RC Metal Works'},
+        {id: _.uniqueId(), name: 'PO #1043 - Alpine Fireplace'}
+    ]);
+app.constant("classifications",
+    [
+        {id: _.uniqueId(), name: 'Labor'},
+        {id: _.uniqueId(), name: 'Materials'},
+        {id: _.uniqueId(), name: 'Equipment'},
+        {id: _.uniqueId(), name: 'General'}
+    ]);
+
+app.constant("disciplines",
+    [
+        id: _.uniqueId(), name: 'Architecture'},
+        id: _.uniqueId(), name: 'Electrical'},
+        id: _.uniqueId(), name: 'Engineering'},
+        id: _.uniqueId(), name: 'Estimating'},
+        id: _.uniqueId(), name: 'HVAC'}
+    ]);
+
+app.constant("invoices",
+    [
+        id: _.uniqueId(), name: 'INV #1029 - Seasons Waterproofing'},
+        id: _.uniqueId(), name: 'INV #1042 - Summit County'}
+    ]);
+
+app.constant("members",
+    [{
+        id: _.uniqueId(),
+        avatar: 'dave.jpg',
+        name: 'Dave Musgrove',
+        company: 'Premier Heating & Air, Inc.',
+        phone: '861-569-6688',
+        availability: 'On Site',
+        next_sched: '18 Sep, 2015',
+        project: 'Wilmington 47',
+        role: 'Sub Contractor',
+        sow: 'Fire Place',
+        flag: {
+            danger: 1,
+            warning: 2
+        }
+    }, {
+        id: _.uniqueId(),
+        avatar: 'mike.jpg',
+        name: 'Mike Riley',
+        company: 'Canyon River Electric',
+        phone: '385-285-8616',
+        availability: 'On Site',
+        next_sched: '18 Sep, 2015',
+        project: 'Wilmington 47',
+        role: 'Sub Contractor',
+        sow: 'Multiple',
+        sow_popover: $sce.trustAsHtml("<div>Main Floor Overhead Lighting<br>Rough Electrical<br>Stairway Floor Lighting</div>"),
+        flag: {
+            warning: 3
+        }
+    }, {
+        id: _.uniqueId(),
+        avatar: 'mary.jpg',
+        name: 'Mary Beroset',
+        company: 'Wasatch Lighting, Inc.',
+        phone: '861-891-1611',
+        availability: 'On Site',
+        show: 'No Show',
+        next_sched: '24 Sep, 2015',
+        project: 'Wilmington 47',
+        role: 'Designer',
+        sow: 'Main Floor Overhead Lighting'
+    }, {
+        id: _.uniqueId(),
+        avatar: 'john.jpg',
+        name: 'John Steel',
+        company: 'L&L Steel Erection',
+        alert: true,
+        phone: '861 518 1636',
+        availability: 'On Site',
+        next_sched: '24 Sep, 2015',
+        project: 'Wilmington 47',
+        role: 'Sub Contractor',
+        sow: 'Fire Place',
+        flag: {
+            danger: 1
+        }
+    }, {
+        id: _.uniqueId(),
+        avatar: 'gebo.jpg',
+        name: 'Dave Gebo',
+        company: 'On Top Roofing',
+        alert: false,
+        phone: '835 513 6185',
+        availability: 'On Site',
+        show: 'No Show',
+        next_sched: '25 Sep, 2015',
+        project: 'Multiple',
+        project_popover: $sce.trustAsHtml("<div>Wilmington 47<br>Wilmington 48</div>"),
+        role: 'Sub Contractor',
+        sow: 'Multiple',
+        sow_popover: $sce.trustAsHtml("<div>Roof Dry-In<br>Set Steel</div>"),
+        flag: {
+            danger: 4,
+            warning: 1
+        }
+    }, {
+        id: _.uniqueId(),
+        avatar: 'dan.jpg',
+        name: 'Dan Bonker',
+        company: 'Sage Creek Framing Co.',
+        alert: true,
+        phone: '861 891 5339',
+        availability: 'Off Site',
+        next_sched: '18 Sep, 2015',
+        project: 'Wilmington 47',
+        role: 'Sub Contractor',
+        sow: 'Frame 2nd Stroy',
+    }, {
+        id: _.uniqueId(),
+        avatar: 'bruce.jpg',
+        name: 'Bruce Wayne',
+        company: 'Synergy Construction',
+        alert: false,
+        phone: '801 580 5683',
+        availability: 'Off Site',
+        next_sched: '18 Sep, 2015',
+        project: 'Multiple',
+        project_popover: $sce.trustAsHtml("<div>Wilmington 45<br>Wilmington 46<br>Wilmington 47<br>Wilmington 48</div>"),
+        role: 'General Contractor',
+        sow: 'Multiple',
+        sow_popover: $sce.trustAsHtml("<div>Frame 2nd Story<br>Insulation<br>Snow Removal</div>"),
+        flag: {
+            danger: 2,
+            warning: 5
+        }
+    }, {
+        id: _.uniqueId(),
+        avatar: 'jeff.jpg',
+        name: 'Jeff Price',
+        company: 'Probuild',
+        alert: false,
+        phone: '835 563 6699',
+        availability: 'Off Site',
+        next_sched: '19 Sep, 2015',
+        project: 'Multiple',
+        project_popover: $sce.trustAsHtml("<div>Wilmington 47<br>Wilmington 48</div>"),
+        role: 'Supplier',
+        sow: 'Multiple',
+        sow_popover: $sce.trustAsHtml("<div>Roof Dry-In<br>Set Steel</div>"),
+        flag: {
+            warning: 3
+        }
+    }, {
+        id: _.uniqueId(),
+        avatar: 'peters.jpg',
+        name: 'Christian Peters',
+        company: 'MyTy Appliance Specialists',
+        alert: false,
+        phone: '861 998 8283',
+        availability: 'Off Site',
+        next_sched: '1 Oct, 2015',
+        project: 'Wilmington 47',
+        role: 'Designer',
+        sow: 'Fire Place',
+    }, {
+        id: _.uniqueId(),
+        avatar: 'jane.jpg',
+        name: 'Jane Smith',
+        alert: false,
+        phone: '801 942 1092',
+        availability: 'Off Site',
+        next_sched: '1 Oct, 2015',
+        project: 'Wilmington 47',
+        role: 'Owner',
+        flag: {
+            warning: 1
+        }
+    }];
+
 app.controller('AppCtrl',
     ['$scope', '$rootScope', '$state', '$localStorage', function ($scope, $rootScope, $state, $localStorage) {
 
