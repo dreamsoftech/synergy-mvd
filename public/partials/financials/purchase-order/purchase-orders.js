00564bb845a7550e7c2a664fd279e65ffd75f212
dreamsoftech <dreamsoftech88@gmail.com>
2016-01-29 11:30:19 -0500

    Modify options data in payment initiate

--- public/partials/financials/purchase-order/purchase-orders.js
+++ public/partials/financials/purchase-order/purchase-orders.js
@@ -1,73 +1,54 @@
-app.controller('PurchaseOrdersCtrl' ,
-    ['$scope', '$state', '$stateParams', '$timeout', function ($scope, $state, $stateParams, $timeout) {
+app.controller('PurchaseOrdersCtrl', function ($scope, members, projects, contracts, sows) {
+    $scope.states = [
+        {
+            id: 1,
+            name: 'AK'
+        }, {
+            id: 2,
+            name: 'AL'
+        }, {
+            id: 3,
+            name: 'CA'
+        }, {
+            id: 4,
+            name: 'NY'
+        }, {
+            id: 5,
+            name: 'TX'
+        }, {
+            id: 6,
+            name: 'FL'
+        }, {
+            id: 7,
+            name: 'HI'
+        }, {
+            id: 8,
+            name: 'VA'
+    }];
 
-        $scope.states = [
-            {
-                id: 1,
-                name: 'AK'
-            }, {
-                id: 2,
-                name: 'AL'
-            }, {
-                id: 3,
-                name: 'CA'
-            }, {
-                id: 4,
-                name: 'NY'
-            }, {
-                id: 5,
-                name: 'TX'
-            }, {
-                id: 6,
-                name: 'FL'
-            }, {
-                id: 7,
-                name: 'HI'
-            }, {
-                id: 8,
-                name: 'VA'
-        }];
-
-        $scope.date = new Date(2015, 7, 26);
+    $scope.date = new Date(2015, 7, 26);
 
 
-        $scope.isTextable = true;
+    $scope.isTextable = true;
+    $scope.users = members;
+    $scope.projects = projects;
+    $scope.contracts = contracts;
+    $scope.sows = sows;
+    $scope.procurements = [
 
-        $scope.projects = [
+    ];
+    $scope.comments = [
+        {
+            avatar: 'bruce.jpg',
+            author: 'Bruce Wayne',
+            date: 'Aug 16, 2015 - 10:29am',
+            desc: 'No issues on my end here. I\'ll let you guy\'s resolve this. Let me know if you need any input!'
+        }, {
+            avatar: 'skylar.jpg',
+            author: 'Christian Spendlove',
+            date: 'Aug 17, 2015 - 9:16am',
+            desc: 'Hi Jane, that should be no problem at all. I will look into the additional costs. It shouldn\'t be a big change...'
+        }
+    ];
 
-        ];
-
-        $scope.contracts = [
-
-        ];
-
-        $scope.sow = [
-
-        ];
-
-        $scope.procurements = [
-
-        ];
-
-        $scope.users = [
-            {
-                id: _.uniqueId(),
-                name: 'Bruce Wayne'
-            }
-        ];
-
-        $scope.comments = [
-            {
-                avatar: 'bruce.jpg',
-                author: 'Bruce Wayne',
-                date: 'Aug 16, 2015 - 10:29am',
-                desc: 'No issues on my end here. I\'ll let you guy\'s resolve this. Let me know if you need any input!'
-            }, {
-                avatar: 'skylar.jpg',
-                author: 'Christian Spendlove',
-                date: 'Aug 17, 2015 - 9:16am',
-                desc: 'Hi Jane, that should be no problem at all. I will look into the additional costs. It shouldn\'t be a big change...'
-            }
-        ];
-
-    }]);
+});
