45f482f5a4fe4906ebe50a1101f2b0f6e37e2d23
dreamsoftech <dreamsoftech88@gmail.com>
2016-01-29 12:00:07 -0500

    Modify direct payment

--- public/partials/financials/direct-payment/direct-payment.js
+++ public/partials/financials/direct-payment/direct-payment.js
@@ -1,103 +1,65 @@
-app.controller('DirectPaymentCtrl',
-    ['$scope', function($scope) {
+app.controller('DirectPaymentCtrl', function($scope, members, projects, contracts, sows, classifications) {
+    $scope.payApplications = [
+        {
+            id: 1,
+            name: 'Demilis'
+        }
+    ];
+    $scope.projects = projects;
+    $scope.contracts = contracts;
+    $scope.sows = sows;
+    $scope.classifications = classifications;
+    $scope.payee = members;
 
-        $scope.projects = [
-            {
-                id: 1,
-                name: "Wood Foullish"
+    $scope.data = [
+        {
+            budgetted: false
+        }
+    ];
+
+    $scope.addLine = function() {
+        $scope.data.push({
+            budgetted: false
+        });
+    };
+
+    $scope.paymentForm = [
+        {
+            id: 1,
+            name: 'Payment Form'
+        }
+    ];
+
+    /** Dropzone config
+    *
+    */
+    $scope.dropzoneConfig = {
+        url: '/upload',
+        parallelUploads: 2,
+        maxFileSize: 30,
+        acceptedFiles: "image/*,application/pdf,application/vnd.openxmlformats-officedocument.wordprocessingml.document,application/xml",
+        clickable: '.btn-file-input',
+        previewTemplate: $('.upload-preview').html(),
+        previewsContainer: '#upload-files-container',
+        accept: function(file) {
+            var thumbnail = $(file.previewElement).find('span.preview img');
+            switch (file.type) {
+                case 'application/pdf':
+                    thumbnail.attr('src', "assets/img/documents/icons/pdf_file.png");
+                    $(file.previewElement).find('span.preview').addClass('not-image');
+                    break;
+                case 'application/vnd.openxmlformats-officedocument.wordprocessingml.document':
+                    thumbnail.attr('src', "assets/img/documents/icons/text_file.png");
+                    $(file.previewElement).find('span.preview').addClass('not-image');
+                    break;
             }
-        ];
+        }
+    };
 
-        $scope.payApplications = [
-            {
-                id: 1,
-                name: 'Demilis'
-            }
-        ];
-
-        $scope.contracts = [
-            {
-                id: 1,
-                name: 'Belsoeis'
-            }
-        ];
-
-        $scope.sows = [
-            {
-                id: 1,
-                name: 'Eaielsels'
-            }
-        ];
-
-
-        $scope.classifications = [
-            {
-                id: 1,
-                name: 'Desates'
-            }
-        ];
-
-        $scope.payee = [
-            {
-                id: 1,
-                name: 'Miles boke'
-            }, {
-                id: 2,
-                name: 'Halm homes'
-            }
-
-        ]
-
-
-        $scope.data = [
-            {
-                budgetted: false
-            }
-        ];
-
-        $scope.addLine = function() {
-            $scope.data.push({
-                budgetted: false
-            });
-        };
-
-        $scope.paymentForm = [
-            {
-                id: 1,
-                name: 'Payment Form'
-            }
-        ];
-
-        /** Dropzone config
-        *
-        */
-        $scope.dropzoneConfig = {
-            url: '/upload',
-            parallelUploads: 2,
-            maxFileSize: 30,
-            acceptedFiles: "image/*,application/pdf,application/vnd.openxmlformats-officedocument.wordprocessingml.document,application/xml",
-            clickable: '.btn-file-input',
-            previewTemplate: $('.upload-preview').html(),
-            previewsContainer: '#upload-files-container',
-            accept: function(file) {
-                var thumbnail = $(file.previewElement).find('span.preview img');
-                switch (file.type) {
-                    case 'application/pdf':
-                        thumbnail.attr('src', "assets/img/documents/icons/pdf_file.png");
-                        $(file.previewElement).find('span.preview').addClass('not-image');
-                        break;
-                    case 'application/vnd.openxmlformats-officedocument.wordprocessingml.document':
-                        thumbnail.attr('src', "assets/img/documents/icons/text_file.png");
-                        $(file.previewElement).find('span.preview').addClass('not-image');
-                        break;
-                }
-            }
-        };
-
-        $scope.dropzoneEventHandlers = {
-            addedfile: function(file) {
-            },
-            thumbnail: function(file, dataUrl) {
-            },
-        };
-    }]);
+    $scope.dropzoneEventHandlers = {
+        addedfile: function(file) {
+        },
+        thumbnail: function(file, dataUrl) {
+        },
+    };
+});
