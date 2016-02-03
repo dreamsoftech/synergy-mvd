d7381ba9b48185e36f089a7476fce78894dd048e
Bryan Lloyd <bryan.lloyd45@gmail.com>
2016-02-03 16:05:52 -0500

    Company Profile - Project Templates Tab

--- public/partials/company-profile/project-templates.js
+++ public/partials/company-profile/project-templates.js
@@ -1,4 +1,16 @@
 app.controller('ComProProjectTemplatesCtrl', function($scope) {
     $scope.$parent.setCurrentTab('company-profile.project-templates');
 
+    $scope.templates = [
+        {name: 'Project Budget Template Name #12345', uploaded_at: 'Feb 12, 2016', uploader: 'John Doe', modified_at: 'Feb 12, 2016', modifier: 'Eric Hoffman'},
+        {name: 'Project Budget Template Name #12345', uploaded_at: 'Feb 12, 2016', uploader: 'John Doe', modified_at: 'Feb 12, 2016', modifier: 'Eric Hoffman'},
+        {name: 'Project Budget Template Name #12345', uploaded_at: 'Feb 12, 2016', uploader: 'John Doe', modified_at: 'Feb 12, 2016', modifier: 'Eric Hoffman'},
+        {name: 'Project Budget Template Name #12345', uploaded_at: 'Feb 12, 2016', uploader: 'John Doe', modified_at: 'Feb 12, 2016', modifier: 'Eric Hoffman'},
+        {name: 'Project Budget Template Name #12345', uploaded_at: 'Feb 12, 2016', uploader: 'John Doe', modified_at: 'Feb 12, 2016', modifier: 'Eric Hoffman'},
+        {name: 'Project Budget Template Name #12345', uploaded_at: 'Feb 12, 2016', uploader: 'John Doe', modified_at: 'Feb 12, 2016', modifier: 'Eric Hoffman'},
+        {name: 'Project Budget Template Name #12345', uploaded_at: 'Feb 12, 2016', uploader: 'John Doe', modified_at: 'Feb 12, 2016', modifier: 'Eric Hoffman'},
+        {name: 'Project Budget Template Name #12345', uploaded_at: 'Feb 12, 2016', uploader: 'John Doe', modified_at: 'Feb 12, 2016', modifier: 'Eric Hoffman'},
+        {name: 'Project Budget Template Name #12345', uploaded_at: 'Feb 12, 2016', uploader: 'John Doe', modified_at: 'Feb 12, 2016', modifier: 'Eric Hoffman'},
+        {name: 'Project Budget Template Name #12345', uploaded_at: 'Feb 12, 2016', uploader: 'John Doe', modified_at: 'Feb 12, 2016', modifier: 'Eric Hoffman'}
+    ];
 });
