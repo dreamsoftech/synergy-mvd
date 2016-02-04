e96ed418f5b619e3f4a84bc29bc81f1a777921b6
Bryan Lloyd <bryan.lloyd45@gmail.com>
2016-02-04 11:53:38 -0500

    Company Profile - Profile Tab

--- public/partials/company-profile/profile.js
+++ public/partials/company-profile/profile.js
@@ -1,3 +1,15 @@
 app.controller('ComProProfileCtrl', function($scope) {
     $scope.$parent.setCurrentTab('company-profile.profile');
+
+    $scope.current_projects = [
+        {name: 'Project name goes here', type: 'Fermentum', sqft: 13000, start_date: 'Aug 23, 2015', est_duration: '3 Months, 0 Days. 1 Year', contingency: '10%'},
+        {name: 'Project name goes here', type: 'Fermentum', sqft: 1234, start_date: 'Aug 23, 2015', est_duration: '3 Months, 0 Days. 1 Year', contingency: '10%'},
+        {name: 'Project name goes here', type: 'Fermentum', sqft: 8000, start_date: 'Aug 23, 2015', est_duration: '3 Months, 0 Days. 1 Year', contingency: '10%'},
+        {name: 'Project name goes here', type: 'Fermentum', sqft: 7850, start_date: 'Aug 23, 2015', est_duration: '3 Months, 0 Days. 1 Year', contingency: '10%'}
+    ];
+
+    $scope.archived_projects = [
+        {name: 'Project name goes here', type: 'Fermentum', comp_date: 'Dec 6, 2014'},
+        {name: 'Project name goes here', type: 'Fermentum', comp_date: 'Dec 6, 2014'}
+    ];
 });
